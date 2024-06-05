70fc2bebd059fc8c5d66bf7dbcdfe60e3b56f99d
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-05 20:30:34 +0100

    Added foreach loop for array

--- part2/module4/Program.cs
+++ part2/module4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] inventory = { 200, 450, 700, 175, 250 };
+
+int sum = 0;
+int bin = 0;
+
+foreach (int item in inventory)
+{
+    sum += item;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
--- part2/module4/obj/Debug/net8.0/module4.AssemblyInfo.cs
+++ part2/module4/obj/Debug/net8.0/module4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("module4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66fe92b2b761f30e9c5ddb448c5e9e6ba0809cd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f17bf6d5751c2e29ff642bff796c56fa14d797c")]
 [assembly: System.Reflection.AssemblyProductAttribute("module4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("module4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
