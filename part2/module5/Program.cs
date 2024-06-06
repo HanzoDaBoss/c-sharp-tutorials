a0809c154a59fbd9c0d6d3dc5784f20066ea70fa
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-06 10:58:05 +0100

    Refactored code

--- part2/module5/Program.cs
+++ part2/module5/Program.cs
@@ -2,18 +2,21 @@
 along with the number of times the letter 'o' appears in it*/
 
 string str = "The quick brown fox jumps over the lazy dog.";
+
 char[] charMessage = str.ToCharArray();
 Array.Reverse(charMessage);
 
-int x = 0;
+int letterCount = 0;
+
 foreach (char i in charMessage)
 {
     if (i == 'o')
     {
-        x++;
+        letterCount++;
     }
 }
+
 string newMessage = new string(charMessage);
 
 Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {x} times.");
+Console.WriteLine($"'o' appears {letterCount} times.");
