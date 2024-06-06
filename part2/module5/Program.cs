f2b04d8d6c08628006714eced344761211f0714a
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-06 10:32:21 +0100

    Added comments to code

--- part2/module5/Program.cs
+++ part2/module5/Program.cs
@@ -1,3 +1,6 @@
 ﻿string firstName = "Hanif";
 int widgetsSold = 7;
+// Testing a change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
 Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
