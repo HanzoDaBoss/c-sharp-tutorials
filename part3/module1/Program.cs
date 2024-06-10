da2de55c881a04eacf9eceaf89078743c5e0a007
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-10 12:35:00 +0100

    Boolean expression condition challenge

--- part3/module1/Program.cs
+++ part3/module1/Program.cs
@@ -27,7 +27,29 @@
 // Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
 
 Random number = new Random();
-
 int flip = number.Next(2);
-
 Console.WriteLine($"The coin landed on: {(flip == 0 ? "heads" : "tails")}");
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin"))
+{
+    if (level >= 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else Console.WriteLine("You do not have sufficient privileges.");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
