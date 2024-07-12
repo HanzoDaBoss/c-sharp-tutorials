659ee334750e4d06f251087981b532036d799ccb
HanzoDaBoss <hanif.uddz@gmail.com>
2024-07-12 09:26:11 +0100

    Removed curly brace from if statement

--- part3/module2/Program.cs
+++ part3/module2/Program.cs
@@ -1,10 +1,3 @@
 ﻿bool flag = true;
-int value = 0;
-
 if (flag)
-{
-    Console.WriteLine($"Inside the code block: {value}");
-}
-
-value = 10;
-Console.WriteLine($"Outside the code block: {value}");
+    Console.WriteLine(flag);
--- part3/module2/obj/Debug/net8.0/module2.AssemblyInfo.cs
+++ part3/module2/obj/Debug/net8.0/module2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("module2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07b7eb668ee05c204e9b28e4f82a7a0ff722a99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a9d94fb6bd3b1e3552ec326a3bf5e683e7488b")]
 [assembly: System.Reflection.AssemblyProductAttribute("module2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("module2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
