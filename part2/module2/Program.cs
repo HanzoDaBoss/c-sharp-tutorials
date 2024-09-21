fde8bb037924df3cb0101fb8c686989f1ef29ef2
HanzoDaBoss <hanif.uddz@gmail.com>
2024-09-21 21:32:37 +0100

    Changed console log part2 module2

--- part2/module2/Program.cs
+++ part2/module2/Program.cs
@@ -2,4 +2,4 @@
 int secondValue = 600;
 int largerValue = Math.Max(firstValue, secondValue);
 
-Console.WriteLine($"The largest value is {largerValue}");
+Console.WriteLine($"The largest value generated is {largerValue}");
